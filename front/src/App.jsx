106478d35f012de058ca34e4e705d4684ced6acd
elroi773 <s2457@e-mirim.hs.kr>
2026-01-04 23:29:26 +0900

    라우트 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,16 +4,20 @@ import SignUp from "./pages/Auth/SignUp";
 import InformationInput from "./pages/Information/InformationInput";
 import OtherPartyInformation from "./pages/Information/OtherPartyInformation";
 import MyPage from "./pages/MyPage/MyPage";
+import Home from "./pages/Home/Home";
+import SplashScreen from "./pages/SplashScreen/SplashScreen";
 
 export default function App() {
   return (
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/splash" replace />} />
+        <Route path="/splash" element={<SplashScreen />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/information" element={<InformationInput />} />
         <Route path="/other-party-information" element={<OtherPartyInformation />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
   );
 }
