2ac43c35d135628e282b05f7d11463ba97e25174
elroi773 <s2457@e-mirim.hs.kr>
2026-01-11 15:15:32 +0900

    백엔드 연동 /미래 배우자 제외

--- front/src/components/auth/AuthButton.jsx
+++ front/src/components/auth/AuthButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./auth.css";
 
-export default function AuthButton({ children, type = "submit", onClick, disabled }) {
+export default function AuthButton({ children, type = "submit", onClick, disabled, style }) {
   return (
-    <button className="authButton" type={type} onClick={onClick} disabled={disabled}>
+    <button className="authButton" type={type} onClick={onClick} disabled={disabled} style={style}>
       {children}
     </button>
   );
